3fe2ee1e95f8fdbd57f84ff8dd4dfff53e9f4b07
yavuzff <yavuzff@outlook.com>
2026-04-16 19:20:53 +0100

    add dataset downloading

---
+++ scripts/install_gptcache_dataset.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# ensure we are in the project root
+CURRENT_DIR=$(basename "$PWD")
+
+if [ "$CURRENT_DIR" != "semantic-layer-skipping" ]; then
+    echo "Error: Script must be run from the 'semantic-layer-skipping' directory."
+    echo "Current directory is: $PWD"
+    exit 1
+fi
+
+echo "Verified project root. Proceeding..."
+
+# create the data directory
+mkdir -p data
+
+cd data || { echo "Failed to enter data directory"; exit 1; }
+
+echo "Downloading datasets from GPTCache repository..."
+
+# download the datasets
+curl -O https://raw.githubusercontent.com/zilliztech/GPTCache/main/examples/benchmark/similiar_qqp.json.gz
+curl -O https://raw.githubusercontent.com/zilliztech/GPTCache/main/examples/benchmark/similiar_qqp_full.json.gz
+
+# unzip the downloaded files (
+gunzip -f similiar_qqp.json.gz
+gunzip -f similiar_qqp_full.json.gz
+
+# output dataset size
+DIR_SIZE=$(du -sh . | awk '{print $1}')
+
+echo "Total size of the 'data' directory: $DIR_SIZE"
